f7c06f7de2283d54577fcc878219bdf3df217f43
Swetabh Mukherjee <swetabh.libra@gmail.com>
2019-10-01 14:00:24 +0530

    Released v1.2

--- setup.sh
+++ setup.sh
@@ -4,11 +4,43 @@
 # Bash script to install and generate pip requirements.txt from inside a virtualenv.
 # usage pip-install package1 package2 package3 .. package(n)... etc
 # 
+
+# START
+
 echo "---------------------------------------------------------------------" 
 echo "Starting the script........" 
 
+sudo apt-get upgrade
+sudo apt upgrade
+sudo apt-get update
+sudo apt update
+sudo apt install docker.io
+#install pip based on os(macOS or linux)
+if sudo apt install python3-pip; then
+    sudo apt install python3-pip
+else
+    brew install python
+fi
 
-pip install virtualenv
+if sudo apt-get -y install python3-pip; then
+    sudo apt-get -y install python3-pip
+else
+    brew install python
+fi
+
+if sudo apt-get install python-pip; then
+    sudo apt-get install python-pip
+else
+    brew install python
+fi
+
+pip install --upgrade pip
+pip3 install --upgrade setuptools pip
+
+sudo apt-get install python-skimage
+
+sudo pip3 install virtualenv
+sudo apt install virtualenv
 virtualenv api
 source ./api/bin/activate
 
@@ -17,12 +49,10 @@ echo "---------------------------------------------------------------------"
 echo "Enter the path of requirements.txt file"
 read requirements_path
 echo "---------------------------------------------------------------------" 
-
 echo "Requested packages are :"
 while read line; do echo $line; done < $requirements_path
 echo "---------------------------------------------------------------------" 
 
-# packages=$(while read line; do echo $line; done < $requirements_path)
 
 echo "Starting to install packages......." 
    #check virtualenv path 
@@ -30,14 +60,14 @@ if [[ "$VIRTUAL_ENV" != "" ]]; then
     echo "You are in a working virtualenv $VIRTUAL_ENV";
    # virtual_env > check if packages is empty .. if [[ ]]; then
     # if [[ "$packages" != "" ]]; then
-    pip install -r $requirements_path;
+    pip3 install -r $requirements_path;
+    pip3 freeze > "additional_requirements.txt"
+echo "---------------------------------------------------------------------"
+echo "Installing additional requirements......."
+echo "---------------------------------------------------------------------"
+
+    pip3 install -r additional_requirements.txt
     
-# virtual_env > packages .. else; fi 
-  # else 
-  #   echo "Please enter at least one package to install";
-  #   exit 1;
-  # fi 
-# virtual_env .. else; fi
 else 
     echo "You are not in a working virtualenv"
     echo "Exiting .........."
